bd91e1c494d030f028e706f5ff14b6c7939d6bb6
cammit17 <kamil.karwacki65@gmail.com>
2020-12-08 02:52:09 +0100

    ALL DONE

--- script.js
+++ script.js
@@ -13,8 +13,9 @@ var tl = gsap.timeline();
 
 //opening menu
 menuOpenButton.addEventListener("click", ev =>{
-    tl.to('.menu-opener', {duration: .5, scale: 0, display: 'none',ease: "power2.out"})
-        .to('.menu-closer', {duration:.5, scale:1, display: 'block',ease: "power2.out"})
+    tl.to('.menu-opener', {duration: .3, scale: 0, display: 'none',ease: "power2.out"})
+        .to('.menu-closer', {duration:.3, scale:1, display: 'block',ease: "power2.out"
+    })
     if(menu.classList.contains('close')){
         menu.classList.remove('close');
         menu.classList.add('open');
@@ -28,15 +29,15 @@ menuOpenButton.addEventListener("click", ev =>{
 
 //closing menu
 menuCloseButton.addEventListener("click", ev=>{
-    tl.to('.menu-closer', {duration: .5, scale: 0, display: 'none', ease: "power2.out"})
-        .to('.menu-opener', {duration:.5, scale:1, display:'block', ease: "power2.out"})
+    tl.to('.menu-closer', {duration: .3, scale: 0, display: 'none', ease: "power2.out"})
+        .to('.menu-opener', {duration:.3, scale:1, display:'block', ease: "power2.out"})
 
     if(menu.classList.contains('open')){
         menu.classList.remove('open');
         menu.classList.add('close');
         body.style.overflowY = 'auto';
 
-        gsap.to('.menu-container', {duration:1, y:"100%", ease: "power2.out"})
+        gsap.to('.menu-container', {duration: .7, y:"100%", ease: "power2.out"})
     }    
 })
 
